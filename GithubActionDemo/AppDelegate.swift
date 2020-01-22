01d269f4ab482d340645753f8147ab2a666d9960
朱宋宇 <>
2020-01-22 17:17:16 +0800

    1、代码提交

---
+++ GithubActionDemo/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  GithubActionDemo
+//
+//  Created by 朱宋宇 on 2020/1/22.
+//  Copyright © 2020 zsy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ GithubActionDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GithubActionDemo
+//
+//  Created by 朱宋宇 on 2020/1/22.
+//  Copyright © 2020 zsy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
